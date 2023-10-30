3ae838f201f79a4edbb10744d4ac67cf861a1af7
JOÃO VÍTOR BORTOLUZ DA ROSA <1134776@ATITUS.LOCAL>
2023-10-30 20:46:27 -0300

    Quarto commit-Trabalho Avaliativo

---
+++ src/main/java/br/edu/atitus/atitusound/controllers/PlaylistController.java
@@ -0,0 +1,39 @@
+package br.edu.atitus.atitusound.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.atitus.atitusound.dtos.PlaylistDTO;
+import br.edu.atitus.atitusound.entities.PlaylistEntity;
+import br.edu.atitus.atitusound.services.GenericServices;
+import br.edu.atitus.atitusound.services.PlaylistService;
+
+@RestController
+@RequestMapping("/playlists")
+public class PlaylistController extends GenericController<PlaylistEntity, PlaylistDTO>{
+	
+	private final PlaylistService service;
+	
+	
+
+	public PlaylistController(PlaylistService service) {
+		super();
+		this.service = service;
+	}
+
+	@Override
+	public GenericServices<PlaylistEntity> getService() {
+		return service;
+	}
+
+	@Override
+	protected PlaylistEntity convertDTO2Entity(PlaylistDTO dto) {
+		PlaylistEntity entity = new PlaylistEntity();
+		entity.setName(dto.getName());
+		entity.setPublic_share(dto.getPublic_share());
+			
+		return entity;
+	}
+	
+
+}
