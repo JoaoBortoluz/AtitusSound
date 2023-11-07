c9de3292ad1fd02daca681f482561cca7e7ec57b
JOÃO VÍTOR BORTOLUZ DA ROSA <1134776@atitus.local>
2023-11-06 21:57:54 -0300

    AuthToken

--- src/main/java/br/edu/atitus/atitusound/controllers/AuthController.java
+++ src/main/java/br/edu/atitus/atitusound/controllers/AuthController.java
@@ -3,18 +3,26 @@ package br.edu.atitus.atitusound.controllers;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.atitus.atitusound.dtos.SigninDTO;
 import br.edu.atitus.atitusound.dtos.UserDTO;
 import br.edu.atitus.atitusound.entities.UserEntity;
 import br.edu.atitus.atitusound.services.UserService;
+import br.edu.atitus.atitusound.utils.JwtUtils;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+	
+	private final UserService service;
+	private final AuthenticationConfiguration auth;
 	
 	private UserEntity convertDTO2Entity(UserDTO dto) {
 		var user = new UserEntity();
@@ -22,11 +30,29 @@ public class AuthController {
 		return user;
 	}
 	
-	private final UserService service;
+	
 
-	public AuthController(UserService service) {
+	public AuthController(UserService service, AuthenticationConfiguration auth) {
 		super();
 		this.service = service;
+		this.auth = auth;
+	}
+	
+	@PostMapping("/signin")
+	public ResponseEntity<String> signin(@RequestBody SigninDTO signin){
+		//Realizar a autenticação
+		try {
+			auth.getAuthenticationManager().authenticate(new UsernamePasswordAuthenticationToken(signin.getUsername(), signin.getPassword()));
+		} catch (AuthenticationException e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).header("error", e.getMessage()).build();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).header("error", e.getMessage()).build();
+		}
+		//Gerar o Token e retornar para o usuário
+		String jwt = JwtUtils.generateTokenFromUsername(signin.getUsername());
+		return ResponseEntity.ok(jwt);
 	}
 	
 	@PostMapping("/signup")
--- src/main/java/br/edu/atitus/atitusound/services/UserService.java
+++ src/main/java/br/edu/atitus/atitusound/services/UserService.java
@@ -1,7 +1,9 @@
 package br.edu.atitus.atitusound.services;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import br.edu.atitus.atitusound.entities.UserEntity;
 
-public interface UserService extends GenericServices<UserEntity>{
+public interface UserService extends GenericServices<UserEntity>, UserDetailsService{
 
 }
